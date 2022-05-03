19b7ce472b2e1c44c237184e4d160c2906b9fb82
Nagez <81879748+Nagez@users.noreply.github.com>
2022-05-03 21:05:50 +0300

    global string and create function

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
+queriesString = ""
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def generateCypherCreate():
+    global queriesString
+    for name in ["Alice", "Bob", "Carol"]:
+        queriesString =queriesString + "CREATE (student:Student name:"+name+") RETURN student\n"
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Learn Path git Welcome good sir')
+    print_hi('Learn Path git. Welcome good sir.')
+    f = open("Cypher.txt", "w")  #"a" - Append - will append to the end of the file, "w" - Write - will overwrite any existing content
+    generateCypherCreate()
+    print("query:\n"+queriesString)
+    f.write(queriesString)
+    #
+    f.close()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+

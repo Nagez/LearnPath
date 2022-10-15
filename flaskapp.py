c38430d3d90f4b3030f342f6b075b7d7372bac23
ShakedWagner <shakedvip@hotmail.com>
2022-10-15 17:27:28 +0300

    Cleared some code. changed button text to "Submit" in LearnPathHome.html

--- flaskapp.py
+++ flaskapp.py
@@ -14,27 +14,19 @@ def home():
 def show():
     if request.method == "GET":
         Institution = learnPath.write_getAllQuery("Institution")
-    return render_template('show.html', list=Institution, typeList="Institution")
+    return render_template('show.html', list=Institution, typeList="Institutions")
 
 
 @app.route('/show/<variable>', methods=["GET"])
 def show2(variable):
     if request.method == "GET":
-        print(variable)
-        print(int(variable))
         if int(variable) > 6:
             res = learnPath.getClassesFromFaculty(variable)
-            typeOfList = list(res[0].labels)[0]
-            #print(res)
-            return render_template('showClass.html', list=res, typeList=str(typeOfList))
+            return render_template('showClass.html', list=res, typeList="Classes")
         else:
             res = learnPath.getFacultiesFromUni(variable)
-            typeOfList = list(res[0].labels)[0]
-            #print("else")
-            #print(res)
-            print(list(res[0].labels)[0])
     print(res)
-    return render_template('show.html', list=res, typeList=str(typeOfList))
+    return render_template('show.html', list=res, typeList="Faculties")
 
 
 @app.route('/showClass', methods=["GET", "POST"])

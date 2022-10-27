c9d4d5d8ad407fcebca3076a40409fe9fa54918e
ShakedWagner <shakedvip@hotmail.com>
2022-10-27 03:07:51 +0300

    - All new custom validation system (changed in all forms)   also added some code in js and css files.  -removed event=0 from GUI  -added new import in basePage.html for the validation icon  -added comments to js file

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,63 +1,122 @@
-//document.getElementById("submit").disabled = true;
-// var nameError = document.getElementById('name-error');
 
 (function () {
+        //prevent loading page in other pages (excluding '/')
+    if (window.location.pathname != '/') {
+        document.body.onload = null;
+        }
   'use strict'
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
+  var forms = document.querySelectorAll('.form-validation')
 
   // Loop over them and prevent submission
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+      form.addEventListener('submit', function (event) {//for each one of the forms found add an EventListener
+        if (!validateFname() || !validateLname() || !validateClass() || !validateID()) { //if the form is not valid
+          event.preventDefault()//prevent the default event to be taken (prevent submission of the form)
+          event.stopPropagation()//prevents further propagation(bubbling up) of the current event (will not go to next event)
         }
-        form.classList.add('was-validated')
+
       }, false)
     })
+})() //Immediately Invoked Function Expressions (function() {})();
 
-})()
 
-//prevent loading page in other pages (excluding '/')
-if (window.location.pathname != '/') {
-  document.body.onload = null;
-}
 
 //fade alerts
     window.setTimeout(function() {
     $(".alert").fadeTo(500, 0)
 }, 7000);
 
-// function validateName (){
-//      var name = document.getElementById('firstName').value
-//      if (name.length == 0){
-//          nameError.innerHTML = 'Name is required'
-//          return false
-//      }
-//      nameError.innerHTML = 'valid'
-//      return true
-//      }
 
-// //form
-// var firstName=document.getElementById("firstName")
-// var lastName=document.getElementById("lastName")
-// var Area=document.getElementById("Area")
-// var Gender=document.getElementById("Gender")
-// var Psychometric=document.getElementById("Psychometric-output")
-// var Bagrut=document.getElementById("Bagrut-output")
-//
-//
-//
-// function printVal(){
-//     //form submit .value
-//     var firstName_Text=firstName.value
-//     var lastName_Text=lastName.value
-//     var Area_Text=Area.options[Area.selectedIndex].text
-//     var Gender_Text=Area.options[Area.selectedIndex].text
-//     var Psychometric_val=Psychometric.value
-//     var Bagrut_val=Bagrut.value
-//
-// }
+//validation for first name
+function validateFname(){
+     var fname = document.getElementById('firstName')
+     var fnameError = document.getElementById('fname-error')
+     //if fname is not null
+     if(fname!=null)
+    {
+        fname=fname.value
+         if (!fname.match(/^([A-Za-z]+)$/))
+         {
+           fnameError.innerHTML = 'invalid first name'
+           return false
+         }
+         fnameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>'
+         return true
+    }
+    else//fname is null
+    {
+       return true
+    }
+}
+
+
+//validation for last name
+function validateLname(){
+    var lname = document.getElementById('lastName')
+    var lnameError = document.getElementById('lname-error');
+    //if lname is not null
+    if(lname!=null)
+    {
+        lname=lname.value
+        if (!lname.match(/^([A-Za-z]+)$/))
+        {
+            lnameError.innerHTML = 'invalid last name'
+            return false
+        }
+         lnameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>'
+         return true
+    }
+    else //lname is null
+    {
+        return true
+    }
+
+}
+
+
+//validation for class name
+function validateClass(){
+    var cname = document.getElementById("class")
+    var cnameError = document.getElementById('cname-error');
+    //if class is not null
+    if(cname!=null)
+    {
+        cname=cname.value
+        if(!cname.match(/^([A-Za-z]+)$/)) //if class is not valid
+        {
+            cnameError.innerHTML = 'invalid class name'
+            return false
+        }
+        cnameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>'
+        return true
+    }
+    else //class is null
+    {
+        return true
+    }
+}
+
+//validation for id
+function validateID(){
+    var id = document.getElementById("ID")
+    var idError = document.getElementById('id-error');
+    //if id is not null
+    if(id!=null)
+    {
+        id=id.value
+        if(!id.match(/(^[1-9]+)([0-9]*)/)) //if class is not valid
+        {
+            idError.innerHTML = 'invalid ID'
+            return false
+        }
+        idError.innerHTML = '<i class="fa-solid fa-circle-check"></i>'
+        return true
+    }
+    else //id is null
+    {
+        return true
+    }
+}
